460c3debf5ec73f0652a496254ad9b5b3622caf7
christine betts <chrstn@uw.edu>
2025-11-04 08:57:15 -0800

    Fix flicker in screen reader nudge (#12541)

--- packages/cli/src/ui/components/Notifications.tsx
+++ packages/cli/src/ui/components/Notifications.tsx
@@ -51,7 +51,7 @@ export const Notifications = () => {
   }, []);
 
   const showScreenReaderNudge =
-    isScreenReaderEnabled && !hasSeenScreenReaderNudge;
+    isScreenReaderEnabled && hasSeenScreenReaderNudge === false;
 
   useEffect(() => {
     const writeScreenReaderNudgeFile = async () => {
