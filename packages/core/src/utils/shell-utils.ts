613b8a452752be855432d02762274e1702648efb
Pyry Takala <7336413+pyrytakala@users.noreply.github.com>
2025-11-20 16:34:47 -0800

    fix(core): correct bash @P prompt transformation detection (#13544)

--- packages/core/src/utils/shell-utils.ts
+++ packages/core/src/utils/shell-utils.ts
@@ -271,7 +271,7 @@ function hasPromptCommandTransform(root: Node): boolean {
         const transformNode = current.child(i + 1);
 
         if (
-          operatorNode?.type === '@' &&
+          operatorNode?.text === '@' &&
           transformNode?.text?.toLowerCase() === 'p'
         ) {
           return true;
